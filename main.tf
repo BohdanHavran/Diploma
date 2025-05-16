aaecaf81d296ed18cfc85298f858c4fd0a203d73
Bohdan Havran <danohavran@gmail.com>
2025-05-16 16:10:00 +0300

    feature: add module mysql and container-registry

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "k8s" {
   cluster_version = "1.32.2-do.1"
   vpc_uuid        = module.vpc.vpc_id
 
-  docker_credentials = module.container-registry.docker_credentials
+  docker_credentials = module.container-registry.docker_credentials[0]
 
   node_pools = {
     default_node = {
