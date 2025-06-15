9f6faea37409fbdc91f88f54eba317b5e5e2d721
Bohdan <danohavran@gmail.com>
2025-06-16 02:59:55 +0300

    feature: modify Footer and Banner

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -68,7 +68,7 @@ const Footer = () => {
             </div>
             <div className="bg-blue-700">
                 <div className="max-w-screen-xl mx-auto py-4 px-5 flex flex-wrap flex-col justify-center sm:flex-row">
-                    <p className="text-white text-sm text-center sm:text-left">© 2024 Bohdan Havran</p>
+                    <p className="text-white text-sm text-center sm:text-left">© 2025 Bohdan Havran</p>
                 </div>
             </div>
         </footer>
--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -14,11 +14,11 @@ const Banner = () => {
 
                         <div className="flex flex-col space-y-2">
                             <img className="w-24" src="../../../assets/banner/medal.png" alt="banner" />
-                            <h1 className="poppins text-gray-700 font-semibold text-3xl lg:text-3xl leading-relaxed">Best Quality <br /> <span className="text-5xl">Medicine in 2024</span></h1>
-                            <p className="text-gray-500 text-light text-sm">Our products are world best product.We sell the real projects.<br/> Welcome to our shop.</p>
+                            <h1 className="poppins text-gray-700 font-semibold text-3xl lg:text-3xl leading-relaxed">Best Quality <br /> <span className="text-5xl">Medicine in 2025</span></h1>
+                            <p className="text-gray-500 text-light text-sm">Our products are world best product.We sell the real projects.<br/> Welcome to our pharmacy.</p>
                         </div>
                         {/* button  */}
-                        <Button className="btn-primary py-3 px-4 poppins w-48 mt-6" text="Explore our shop" onClick={() => history.push('/products')} />
+                        <Button className="btn-primary py-3 px-4 poppins w-48 mt-6" text="Explore our pharmacy" onClick={() => history.push('/products')} />
                     </div>
                 </Fade>
 
