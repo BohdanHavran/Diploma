a17d3d87350684503f0da8098b55246989d1370b
Bohdan <danohavran@gmail.com>
2025-06-15 20:52:00 +0300

    feature: modify app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, send_file, send_from_directory
+import boto3
 import os
 from flask_jwt_extended import (
     JWTManager, create_access_token, create_refresh_token,
